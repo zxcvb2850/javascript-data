68582202530cb5e1db28bd7c1cc3f4211933575b
zxcvb2850 <zxcvb2850@163.com>
2022-03-07 00:03:19 +0800

    fix: code bug

--- sortBy/bubbleSort.ts
+++ sortBy/bubbleSort.ts
@@ -4,7 +4,7 @@
 const bubbleSort = <T>(nums: T[]): T[] => {
     const len = nums.length;
     for (let i = 0; i < len - 1; i++) {
-        for (let j = 0; j < len - i - i; j++) {
+        for (let j = 0; j < len - 1 - i; j++) {
             const n1 = nums[j];
             const n2 = nums[j + 1];
             if (n1 > n2) {
