ea69713a9e56c38e489e0edc522a0fb1e39c6383
zxcvb2850 <zxcvb2850@163.com>
2022-04-05 23:40:59 +0800

    style: 优化 promise all 参数非 Promise 的实例

--- promise/all.ts
+++ promise/all.ts
@@ -9,19 +9,19 @@ const p2 = new Promise(resolve => {
 const p3 = new Promise((resolve, reject) => resolve(3));
 // const p3 = new Promise((resolve, reject) => reject(3));
 
-Promise.all([p1, p2, p3]).then(res => {
+Promise.all([p1, p2, 4]).then(res => {
     console.log("res", res);
 }).catch(err => {
     console.log("err", err);
 })
 
-const promiseMyAll = (values: Promise<any>[]) => {
+const promiseMyAll = (values: (Promise<any> | number)[]) => {
     return new Promise((resolve, reject) => {
         const result = [];
         let index = 0;
 
         values.forEach((item, i) => {
-            item.then(res => {
+            Promise.resolve(item).then(res => {
                 index++;
                 result[i] = res;
                 if (index === values.length) {
@@ -32,7 +32,7 @@ const promiseMyAll = (values: Promise<any>[]) => {
     })
 }
 
-const result = promiseMyAll([p1, p2, p3]);
+const result = promiseMyAll([p1, p2, 4]);
 result.then(res => {
     console.log("--res--", res);
 }).catch(err => {
